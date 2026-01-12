6e7f985b6458b2dfcc64197053a8491da622a668
Mohamed Ben Taher <mbentaher1998@gmail.com>
2026-01-12 17:56:03 +0000

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: ["*", "3rtgpl-5174.csb.app"],
+    allowedHosts: true,
   },
 });
